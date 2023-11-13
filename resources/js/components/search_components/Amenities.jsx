4ae84097d5f7a0be7bd1c789604b21f2edc9a881
Marcel Balint <marcelbalint363@gmail.com>
2023-11-13 15:51:54 +0200

    Search components

---
+++ resources/js/components/search_components/Amenities.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ConstructionType = () => {
+    return (
+        <div className="amenities-btns">
+            <button>Balcony/Terrace</button>
+            <button>Wheelchair Accesible</button>
+            <button>Basement</button>
+            <button>Privete Parking</button>
+            <button>Garden</button>
+        </div>
+    );
+};
+
+export default ConstructionType;
---
+++ resources/js/components/search_components/Dispositions.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Dispositions = () => {
+    return (
+        <div className="dispositons-btns">
+            <button>1kk</button>
+            <button>1+1</button>
+            <button>2kk</button>
+            <button>2+1</button>
+            <button>3kk</button>
+            <button>3+1</button>
+            <button>4kk</button>
+            <button>Bigger</button>
+        </div>
+    );
+};
+
+export default Dispositions;
