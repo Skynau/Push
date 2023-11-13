cfd77bdc7acd320c5bbf7d7a314a2215b5cfed55
Marcel Balint <marcelbalint363@gmail.com>
2023-11-13 16:58:08 +0200

    Small change

--- resources/js/components/search_components/BudgetSelect.jsx
+++ resources/js/components/search_components/BudgetSelect.jsx
@@ -14,18 +14,17 @@ const Budget = () => {
                 <option value="35.000.00">35.000.00</option>
             </section>
             <section>
-                <option defaultChecked="TO CZK">
-                    <option value="15.000.00">15.000.00</option>
-                    <option value="20.000.00">20.000.00</option>
-                    <option value="25.000.00">25.000.00</option>
-                    <option value="30.000.00">30.000.00</option>
-                    <option value="35.000.00">35.000.00</option>
-                    <option value="40.000.00">40.000.00</option>
-                    <option value="50.000.00">50.000.00</option>
-                    <option value="60.000.00">60.000.00</option>
-                    <option value="40.000.00">40.000.00</option>
-                    <option value="more">More</option>
-                </option>
+                <option defaultChecked="TO CZK"> </option>
+                <option value="15.000.00">15.000.00</option>
+                <option value="20.000.00">20.000.00</option>
+                <option value="25.000.00">25.000.00</option>
+                <option value="30.000.00">30.000.00</option>
+                <option value="35.000.00">35.000.00</option>
+                <option value="40.000.00">40.000.00</option>
+                <option value="50.000.00">50.000.00</option>
+                <option value="60.000.00">60.000.00</option>
+                <option value="40.000.00">40.000.00</option>
+                <option value="more">More</option>
             </section>
         </div>
     );
