fd592eb09d6674da38bd90c22c9c2a0ed1cbd26f
Daniel Slezak <danielslezaak@gmail.com>
2023-11-14 14:34:31 +0100

    'results'

---
+++ resources/js/components/results_components/TypeResults.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TypeResults = () => {
+  return (
+    <>
+            <input type="checkbox" name="apartment" id="apartment" {state.ischecked? 'checked': ''}/>
+            <label htmlFor="apartment">Apartment</label>
+            <input type="checkbox" name="house" id="house" />
+            <label htmlFor="house">House</label>
+    </>
+  )
+}
+
+export default TypeResults
