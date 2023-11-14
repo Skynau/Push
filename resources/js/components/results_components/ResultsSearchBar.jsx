8bda4aa8c3fe88edf041a0ae8dedfefef6ed390b
Daniel Slezak <danielslezaak@gmail.com>
2023-11-14 15:46:41 +0100

    continuing with results page

--- resources/js/components/results_components/ResultsSearchBar.jsx
+++ resources/js/components/results_components/ResultsSearchBar.jsx
@@ -4,7 +4,9 @@ import DispositionResults from './DIspositionResults'
 
 const ResultsSearchBar = () => {
 
-  const [type, setType] = useState(false);
+  const [type, setType] = useState([
+    
+  ]);
   const [disposition, setDisposition] = useState(false);
   const [priceFrom, setPriceFrom] = useState(false);
   const [priceTo, setPriceTo] = useState(false);
@@ -22,6 +24,7 @@ const ResultsSearchBar = () => {
     setAreaFrom(false);
     setAreaTo(false);
     setOther(false);
+  }
 
   const displayDisposition = () => {
     setType(false);
@@ -32,6 +35,73 @@ const ResultsSearchBar = () => {
     setAreaFrom(false);
     setAreaTo(false);
     setOther(false);
+  }
+
+  const displayPriceFrom = () => {
+    setType(false);
+    setDisposition(true);
+    setPriceFrom(false);
+    setPriceTo(false);
+    setLocation(false);
+    setAreaFrom(false);
+    setAreaTo(false);
+    setOther(false);
+  }
+
+  const displayPriceTo = () => {
+    setType(false);
+    setDisposition(true);
+    setPriceFrom(false);
+    setPriceTo(false);
+    setLocation(false);
+    setAreaFrom(false);
+    setAreaTo(false);
+    setOther(false);
+  }
+
+  const displayLocation = () => {
+    setType(false);
+    setDisposition(true);
+    setPriceFrom(false);
+    setPriceTo(false);
+    setLocation(false);
+    setAreaFrom(false);
+    setAreaTo(false);
+    setOther(false);
+  }
+
+  const displayAreaFrom = () => {
+    setType(false);
+    setDisposition(true);
+    setPriceFrom(false);
+    setPriceTo(false);
+    setLocation(false);
+    setAreaFrom(false);
+    setAreaTo(false);
+    setOther(false);
+  }
+
+  const displayAreaTo = () => {
+    setType(false);
+    setDisposition(true);
+    setPriceFrom(false);
+    setPriceTo(false);
+    setLocation(false);
+    setAreaFrom(false);
+    setAreaTo(false);
+    setOther(false);
+  }
+
+  const displayOther = () => {
+    setType(false);
+    setDisposition(true);
+    setPriceFrom(false);
+    setPriceTo(false);
+    setLocation(false);
+    setAreaFrom(false);
+    setAreaTo(false);
+    setOther(false);
+  }  
   
 
   
@@ -39,14 +109,14 @@ const ResultsSearchBar = () => {
     <>
       <button onClick={displayType}>Type</button>
       <button onClick={displayDisposition}></button>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
+      <button onClick={displayPriceFrom}></button>
+      <button onClick={displayPriceTo}></button>
+      <button onClick={displayLocation}></button>
+      <button onClick={displayAreaFrom}></button>
+      <button onClick={displayAreaTo}></button>
+      <button onClick={displayOther}></button>
       <form action="">
-          <div className={`search-bar_type` + ${type} ? 'active' : ''}>
+          <div className={`search-bar_type ${array.type ? " active" : ""}`}>
             <TypeResults />
           </div>
           <div className="search-bar_disposition">
