8cb737a29f2f0558b6391f8a20be26012ce0f027
Marcel Balint <marcelbalint363@gmail.com>
2023-11-14 16:48:38 +0200

    Create the url for the get  request with the values form state

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,58 +1,24 @@
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import SearchContainer from "./SearchContainer";
 import Navigation from "./Navigation";
 import reducer from "../reducer";
 import Context from "../Context";
+import state from "../../js/state";
 
 const App = () => {
-    const contextState = {
-        filterOptions: {
-            apartment: false,
-            house: false,
-            "1kk": false,
-            "1+1": false,
-            "2kk": false,
-            "2+1": false,
-            "3kk": false,
-            "3+1": false,
-            "4kk": false,
-            bigger: false,
-            amountFrom: null,
-            amountTo: null,
-            searchFieldValue: "",
-            sizeFrom: null,
-            sizeTo: null,
-            furnished: false,
-            partialyFurnished: false,
-            unfurnished: false,
-            balconyOrTerrace: false,
-            wheelchairAccessible: false,
-            basement: false,
-            privateParking: false,
-            garden: false,
-            petsWelcome: false,
-            datePicker: null,
-        },
-    };
+    const [contextValue, setContextValue] = useReducer(reducer, state);
 
-    const [contextValue, setContextValue] = useReducer(reducer, contextState);
-
-    const url = `/api/search?type%${
-        contextState.filterOptions.apartment ? "apartment" : ""
-    }&disposition%2kk${
-        contextState.filterOptions["1+1"] ? "1kk" : ""
-    }&petsWelcome%true`;
-    console.log(url);
-    const filterOptions = contextState.filterOptions;
-
-    const urlTest = `/api/search?${Object.entries(filterOptions)
-        .filter(([key, value]) => value) // only truthy values
-        .map(
-            ([key, value]) =>
-                `${encodeURIComponent(key)}=${encodeURIComponent(value)}` // key=value biuld here
-        )
-        .join("&")}`;
-    console.log(urlTest);
+    useEffect(() => {
+        const filterOptions = state.filterOptions;
+        const urlTest = `/api/search?${Object.entries(filterOptions)
+            .filter(([key, value]) => value) // only truthy values
+            .map(([key, value]) => {
+                return `${encodeURIComponent(key)}=${encodeURIComponent(
+                    value
+                )}`; // key=value biuld here
+            })
+            .join("&")}`;
+    }, [contextValue]);
 
     return (
         <>
