7e2f54f70df44d35f33627f19ad9a22ee37e1a24
Daniel Slezak <danielslezaak@gmail.com>
2023-11-14 16:45:16 +0100

    results components adjusted

---
+++ resources/js/components/results_components/AreaToResults.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AreaToResults = () => {
+  return (
+    <>
+        <select>
+            <option defaultValue="TO m2">TO m2 </option>
+            <option value="30">60m2</option>
+            <option value="40">70m2</option>
+            <option value="50">80m2</option>
+            <option value="60">60m2</option>
+            <option value="70">70m2</option>
+            <option value="80">80m2</option>
+            <option value="90">90m2</option>
+            <option value="100">100m2</option>
+            <option value="more">More</option>
+        </select>
+    </>
+  )
+}
+
+export default AreaToResults
