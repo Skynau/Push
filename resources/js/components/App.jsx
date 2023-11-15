6c8ef4ab2a31084b6f2143d1821dba3b1d086032
Marcel Balint <marcelbalint363@gmail.com>
2023-11-15 21:21:52 +0200

    Created the API call

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -6,6 +6,7 @@ import reducer from "../reducer";
 import Context from "../Context";
 import state from "../../js/state";
 import ResultsOfSearch from "./ResultsOfSearch";
+import { getProperties } from "../helpers";
 
 const App = () => {
     const [contextValue, setContextValue] = useReducer(reducer, state);
@@ -13,7 +14,7 @@ const App = () => {
     useEffect(() => {
         // Bould url dynamically based on user's filtering options
         const filterOptions = state.filterOptions;
-        const urlTest = `/api/search?${Object.entries(filterOptions)
+        const url = `/api/search?${Object.entries(filterOptions)
             .filter(([key, value]) => value) // only truthy values
             .map(([key, value]) => {
                 // Format the date object to YYYY-MM-DD
@@ -31,7 +32,19 @@ const App = () => {
                 )}`; // key=value biuld here
             })
             .join("&")}`;
-        console.log(urlTest);
+
+        // Api call based on user's filtering options
+        const fetchData = async () => {
+            try {
+                const data = await getProperties(url);
+                console.log(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+        console.log(url);
     }, [contextValue]);
 
     return (
