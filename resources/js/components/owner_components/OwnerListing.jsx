d535fdb8013cad189b07c033057171ecbf082f56
Marcel Balint <marcelbalint363@gmail.com>
2023-11-17 21:25:39 +0200

    Created the modal which contains the edit profile form

--- resources/js/components/owner_components/OwnerListing.jsx
+++ resources/js/components/owner_components/OwnerListing.jsx
@@ -9,39 +9,39 @@ import "./OwnerListing.scss";
 
 const OwnerListing = () => {
     return (
-        <div class="house-item">
-            <div class="house-box">
-                <div class="house-item-img">
+        <div className="house-item">
+            <div className="house-box">
+                <div className="house-item-img">
                     <img
                         src="https://image.cnbcfm.com/api/v1/image/103500764-GettyImages-147205632-2.jpg?v=1691157601"
                         alt="house image"
                     />
                 </div>
 
-                <div class="house-item-detail">
-                    <p class="house-item-detail-title">City</p>
-                    <p class="house-item-detail-price">1.000.000.00 CZK</p>
-                    <p class="house-item-detail-address">Address here</p>
+                <div className="house-item-detail">
+                    <p className="house-item-detail-title">City</p>
+                    <p className="house-item-detail-price">1.000.000.00 CZK</p>
+                    <p className="house-item-detail-address">Address here</p>
 
-                    <p class="house-item-detail-icons">
-                        <span class="bed">
+                    <p className="house-item-detail-icons">
+                        <span className="bed">
                             <img src={bedIcon} alt="bed" /> <strong>4</strong>
                         </span>
-                        <span class="bath">
+                        <span className="bath">
                             <img src={bathIcon} alt="bath" /> <strong>6</strong>
                         </span>
-                        <span class="size">
+                        <span className="size">
                             <img src={sizeIcon} alt="size" />{" "}
                             <strong> 44m2</strong>
                         </span>
                     </p>
                 </div>
             </div>
-            <div class="house-item-edit" v-if="house.madeByMe">
-                <span class="item-edit">
+            <div className="house-item-edit">
+                <span className="item-edit">
                     <img src={editIcon} alt="edit" />
                 </span>
-                <span class="item-delete">
+                <span className="item-delete">
                     <img src={deleteIcon} alt="delete" />
                 </span>
             </div>
