9d593e4cf3928a6b6fa1d68533b1d125e064bf51
Marcel Balint <marcelbalint363@gmail.com>
2023-11-17 13:06:22 +0200

    Search functionality on home page and results page

--- resources/js/reducer.js
+++ resources/js/reducer.js
@@ -65,7 +65,9 @@ export default function reducer(state, action) {
         case "user":
             newState.user = action.payload;
             break;
-
+        case "fetchOnResultsPage":
+            newState.fetchOnResultsPage = !newState.fetchOnResultsPage;
+            break;
         default:
             return state;
     }
