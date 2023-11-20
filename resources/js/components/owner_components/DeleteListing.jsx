1e8ed72cae5e552a3e63747fa9076ffdf518539f
Ilona Kny <ili@gonuts.cz>
2023-11-20 11:08:16 +0100

    Fixing delete property

--- resources/js/components/owner_components/DeleteListing.jsx
+++ resources/js/components/owner_components/DeleteListing.jsx
@@ -1,16 +1,18 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 
-const DeleteListing = ({listingId}) => {
+const DeleteListing = ({listingId, loadData }) => {
   const [ message, setMessage ] = useState(null)
+
 
   // console.log(listingId)
 
   const handleSubmit = async (event) => {
-    e.preventDefault();
+    event.preventDefault();
     try {
       const response = await axios.post(`api/property/${listingId}/delete`);
       setMessage(response.data['message']);
+      loadData()
     } catch (error) {
       console.log(error)
     }
@@ -20,9 +22,7 @@ const DeleteListing = ({listingId}) => {
 
   return (<>
   { message 
-    ?
-    {message}
-    :
+    ??
     <form onSubmit={ handleSubmit }>
     <button type="submit">Yes, Delete</button>
     </form>
