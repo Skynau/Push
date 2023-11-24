6f9ebdc5bf7992c621f83ae4b239d6e923323868
Ilona Kny <ili@gonuts.cz>
2023-11-24 09:02:14 +0100

    styling

--- resources/js/components/property_detail_components/Pano.jsx
+++ resources/js/components/property_detail_components/Pano.jsx
@@ -9,11 +9,11 @@ const Pano = ({pano}) => {
       {/* <iframe className="pano" allowFullScreen src="https://cdn.pannellum.org/2.5/pannellum.htm#panorama=https%3A//i.imgur.com/6h98KYI.jpg&autoLoad=true"></iframe> */}
       <Pannellum
         width="100%"
-        height="500px"
+        height="800px"
         image={pano}//THIS WILL BE DYMANIC path for pano from upload
-        // pitch={10}
-        // yaw={180}
-        // hfov={110}
+        pitch={10}
+        yaw={180}
+        hfov={110}
         autoLoad
         showZoomCtrl={false}
       />
