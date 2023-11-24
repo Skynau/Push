067f97e1ab463b213d3c1038f3cba6b1d979ef46
Ilona Kny <ili@gonuts.cz>
2023-11-24 15:34:48 +0100

    fix 360

--- resources/js/components/property_detail_components/Pano.jsx
+++ resources/js/components/property_detail_components/Pano.jsx
@@ -8,8 +8,8 @@ const Pano = ({pano}) => {
     <div>
       {/* <iframe className="pano" allowFullScreen src="https://cdn.pannellum.org/2.5/pannellum.htm#panorama=https%3A//i.imgur.com/6h98KYI.jpg&autoLoad=true"></iframe> */}
       <Pannellum
-        width="100%"
-        height="800px"
+        width="80%"
+        height="900px"
         image={pano}//THIS WILL BE DYMANIC path for pano from upload
         // pitch={10}
         yaw={100}
